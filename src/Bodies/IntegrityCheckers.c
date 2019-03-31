f9a1136ab5a0b3d4bfe4d04863283dff214029ab
noah whitcomb <37048467+Noah-Whitcomb@users.noreply.github.com>
2019-03-31 16:03:03 -0400

    removed kenny shit

--- src/Bodies/IntegrityCheckers.c
+++ src/Bodies/IntegrityCheckers.c
@@ -15,29 +15,6 @@ void readSuperBlock(VDIFile* vdi)
     fread((void*)&vdi->superPage->inodespergroup, 4, 1, vdi->f);
     fread((void*)&vdi->superPage->lastmounttime, 4, 1, vdi->f);
     fread((void*)&vdi->superPage->lastwritetime, 4, 1, vdi->f);
-    //number of times mounted since last consistency
     fread((void*)&vdi->superPage->timesmounted, 2, 1, vdi->f);
-    // number of mounts allowed before consistency check
-    uint16_t mountsallowed;
-    // should be 0xEF53
-    uint16_t magic;
-    // 1 means clean, 2 means system has errors
-    uint16_t systemstate;
-    // what to do when there is an error
-    // 1 - ignore error, 2 - remount file system as read only, 3 - kernel panic
-    uint16_t errorhandler;
-    // combine with major portion to get full version
-    uint16_t minorversion;
-    // POSIX time of last consistency check
-    uint32_t lastcheck;
-    // interval between forced consistency checks
-    uint32_t interval;
-    // 0 - linux, 1 - GNU HURD, 2 - MASIX, 3 - FREEBSD, 4 - other "lites"
-    uint32_t opsysid;
-    uint32_t majorportion;
-    // user id that can use reserved blocks
-    uint16_t userid;
-    // group id that can use reserved blocks
-    uint16_t groupid;
 }
 
