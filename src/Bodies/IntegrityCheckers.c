46f3758515acb5fc1bb789b1711d103aecd0e49f
Kenny Diogo <kenneth.diogo@gmail.com>
2019-03-31 16:02:46 -0400

    - Deleted Noah's shit

--- src/Bodies/IntegrityCheckers.c
+++ src/Bodies/IntegrityCheckers.c
@@ -2,21 +2,6 @@
 
 void readSuperBlock(VDIFile* vdi)
 {
-    uint32_t totalinodes; fread((void*)vdi->superPage->totalinodes, 4, 1, vdi->f);
-    uint32_t totalpages;
-    uint32_t superuserpages;
-    uint32_t unallocatedpages;
-    uint32_t unallocatedinodes;
-    uint32_t superpageknumber;
-    uint32_t log2pagesize;
-    uint32_t log2fragmentsize;
-    uint32_t pagespergroup;
-    uint32_t fragmentspergroup;
-    uint32_t inodespergroup;
-    uint32_t lastmounttime;
-    uint32_t lastwritetime;
-    //number of times mounted since last consistency check
-    uint16_t timesmounted;
     fread((void*)&vdi->superPage->mountsallowed, 2, 1, vdi->f);
     fread((void*)&vdi->superPage->magic, 2, 1, vdi->f);
     fread((void*)&vdi->superPage->systemstate, 2, 1, vdi->f);
