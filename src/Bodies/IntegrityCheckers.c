3b13088c59fcbd85fc31fdcb6c0c04b9be784da8
Kenny Diogo <kenneth.diogo@gmail.com>
2019-03-31 15:55:00 -0400

    - Added half of superblock reads

--- src/Bodies/IntegrityCheckers.c
+++ src/Bodies/IntegrityCheckers.c
@@ -17,27 +17,16 @@ void readSuperBlock(VDIFile* vdi)
     uint32_t lastwritetime;
     //number of times mounted since last consistency check
     uint16_t timesmounted;
-    // number of mounts allowed before consistency check
-    uint16_t mountsallowed;
-    // should be 0xEF53
-    uint16_t magic;
-    // 1 means clean, 2 means system has errors
-    uint16_t systemstate;
-    // what to do when there is an error
-    // 1 - ignore error, 2 - remount file system as read only, 3 - kernel panic
-    uint16_t errorhandler;
-    // combine with major portion to get full version
-    uint16_t minorversion;
-    // POSIX time of last consistency check
-    uint32_t lastcheck;
-    // interval between forced consistency checks
-    uint32_t interval;
-    // 0 - linux, 1 - GNU HURD, 2 - MASIX, 3 - FREEBSD, 4 - other "lites"
-    uint32_t opsysid;
-    uint32_t majorportion;
-    // user id that can use reserved blocks
-    uint16_t userid;
-    // group id that can use reserved blocks
-    uint16_t groupid;
+    fread((void*)&vdi->superPage->mountsallowed, 2, 1, vdi->f);
+    fread((void*)&vdi->superPage->magic, 2, 1, vdi->f);
+    fread((void*)&vdi->superPage->systemstate, 2, 1, vdi->f);
+    fread((void*)&vdi->superPage->errorhandler, 2, 1, vdi->f);
+    fread((void*)&vdi->superPage->minorversion, 2, 1, vdi->f);
+    fread((void*)&vdi->superPage->lastcheck, 4, 1, vdi->f);
+    fread((void*)&vdi->superPage->interval, 4, 1, vdi->f);
+    fread((void*)&vdi->superPage->opsysid, 4, 1, vdi->f);
+    fread((void*)&vdi->superPage->majorportion, 4, 1, vdi->f);
+    fread((void*)&vdi->superPage->userid, 2, 1, vdi->f);
+    fread((void*)&vdi->superPage->groupid, 2, 1, vdi->f);
 }
 
