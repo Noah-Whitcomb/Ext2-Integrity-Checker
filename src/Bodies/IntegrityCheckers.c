c3f29d769fa17e1adba08e14a349fd88e761935f
noah whitcomb <37048467+Noah-Whitcomb@users.noreply.github.com>
2019-05-01 23:39:14 -0400

    hopefully finished with bitmaps

--- src/Bodies/IntegrityCheckers.c
+++ src/Bodies/IntegrityCheckers.c
@@ -30,7 +30,7 @@ void makeBitmaps(VDIFile* vdi)
     // mark off blocks 0, 1, 2 from block group 0
     for(size_t j = 0; j<3; j++)
     {
-        addBlock(vdi, bitmaps, j);
+        addBlock(vdi, bitmaps, j + vdi->superBlock->superBlockNumber);
     }
 
     //check copies of superblock and block group descriptor tables
@@ -41,7 +41,7 @@ void makeBitmaps(VDIFile* vdi)
     {
         if(checkPowerOf(3, i) || checkPowerOf(5, i) || checkPowerOf(7, i))
         {
-            for(size_t j = 0; j<3; j++)
+            for(size_t j = 1; j<4; j++)
             {
                 addBlock(vdi, bitmaps, i*vdi->superBlock->blocksPerGroup + j);
             }
@@ -137,6 +137,7 @@ void addInode(VDIFile* vdi, Bitmaps* bitmaps, uint32_t iNodeNumber)
     uint32_t bitIndex = blockGroupIndex%8;
 
     uint8_t temp = 1u << bitIndex;
+    //if(bitIndex == 0) byteIndex--;
     bitmaps->iNodeBitmaps[blockGroup][byteIndex] |= temp;
 
 }
@@ -176,6 +177,10 @@ void markSingles(VDIFile* vdi, Bitmaps* bitmaps, uint32_t blockNumber)
     uint8_t data[vdi->superBlock->blockSize];
     fetchBlock(vdi, data, blockNumber);
 
+    if(blockNumber == 2766)
+    {
+        printBytes(data, 1024, "block 2766");
+    }
     for(size_t i = 0; i<vdi->superBlock->blockSize/4; i++)
     {
         uint32_t temp;
@@ -191,6 +196,11 @@ void markDouble(VDIFile* vdi, Bitmaps* bitmaps, uint32_t blockNumber)
 {
     uint8_t data[vdi->superBlock->blockSize];
     fetchBlock(vdi, data, blockNumber);
+
+    if(blockNumber == 530)
+    {
+        printBytes(data, 1024, "block 530");
+    }
 
     for(size_t i = 0; i<vdi->superBlock->blockSize/4; i++)
     {
@@ -223,17 +233,21 @@ void markTriple(VDIFile* vdi, Bitmaps* bitmaps, uint32_t blockNumber)
 
 void addBlock(VDIFile* vdi, Bitmaps* bitmaps, uint32_t blockNumber)
 {
-    if(blockNumber == 561)
+    if(blockNumber == 130048)
     {
         int n = 5;
         printf("%d", n);
     }
+    // WHY DOES THIS WORK????
+    blockNumber -= vdi->superBlock->superBlockNumber;
+    // this takes away almost all of out problems but I hate it
     uint32_t blockGroup = blockNumber/vdi->superBlock->blocksPerGroup;
-    uint32_t blockGroupIndex = blockNumber%vdi->superBlock->blocksPerGroup;
-    uint32_t byteIndex = blockGroupIndex/8;
-    uint32_t bitIndex = blockGroupIndex%8;
+    uint32_t blockIndex = blockNumber%vdi->superBlock->blocksPerGroup;
+    uint32_t byteIndex = blockIndex/8;
+    uint32_t bitIndex = blockIndex%8;
 
     uint8_t temp = 1u << bitIndex;
+    uint8_t t = bitmaps->blockBitmaps[blockGroup][byteIndex];
     bitmaps->blockBitmaps[blockGroup][byteIndex] |= temp;
 }
 
@@ -268,6 +282,7 @@ int bitmapsCmp(VDIFile *vdi, Bitmaps *bitmaps, struct List *inodesNotReachable, 
                        bitmaps->iNodeBitmaps[blockGroup][byteIndex], byteIndex, blockGroup, 8) == 0)
             {
                 good = 0;
+                printf("bad inodes!\n");
             }
         }
     }
@@ -278,14 +293,18 @@ int bitmapsCmp(VDIFile *vdi, Bitmaps *bitmaps, struct List *inodesNotReachable, 
     uint8_t freeBlockBitmap[1024];
     fetchBlock(vdi, freeBlockBitmap, vdi->blockGroupDescriptorTable[vdi->superBlock->numBlockGroups-1]->blockUsageBitmap);
 
-    uint32_t numBlocks = vdi->superBlock->totalBlocks%vdi->superBlock->blocksPerGroup;
+    uint32_t numBlocks = vdi->superBlock->totalBlocks%vdi->superBlock->blocksPerGroup-vdi->superBlock->superBlockNumber;
     uint32_t numBytesBlocks = numBlocks/8;
     uint32_t bitsInLastBlockByte = numBlocks%8;
 
-    if(bitsInLastBlockByte == 0) bitsInLastBlockByte = 8;
-    for(uint32_t byteIndex = 0; byteIndex<numBytesBlocks; byteIndex++)
+    printf("##################################\n");
+    printBytes(freeBlockBitmap, numBytesBlocks+1, "original");
+    printBytes(bitmaps->blockBitmaps[vdi->superBlock->numBlockGroups-1], numBytesBlocks+1, "new");
+
+   // if(bitsInLastBlockByte == 0) bitsInLastBlockByte = 8;
+    for(uint32_t byteIndex = 0; byteIndex<=numBytesBlocks; byteIndex++)
     {
-        if(byteIndex == numBytesBlocks-1)
+        if(byteIndex == numBytesBlocks)
         {
             if(bitsCmpBlocks(vdi, blocksNotReachable, blocksReachable, freeBlockBitmap[byteIndex],
                        bitmaps->blockBitmaps[vdi->superBlock->numBlockGroups - 1][byteIndex], byteIndex,
@@ -305,7 +324,7 @@ int bitmapsCmp(VDIFile *vdi, Bitmaps *bitmaps, struct List *inodesNotReachable, 
     uint32_t numInodes = vdi->superBlock->totalInodes%vdi->superBlock->inodesPerGroup;
     uint32_t numBytesInodes = numInodes/8;
     uint32_t bitsInLastInodeByte = numInodes%8;
-    for(uint32_t byteIndex = 0; byteIndex<numBytesInodes; byteIndex++)
+    for(uint32_t byteIndex = 0; byteIndex<=numBytesInodes; byteIndex++)
     {
         if(byteIndex == numBytesInodes-1)
         {
@@ -321,6 +340,7 @@ int bitmapsCmp(VDIFile *vdi, Bitmaps *bitmaps, struct List *inodesNotReachable, 
                         vdi->superBlock->numBlockGroups - 1, 8) == 0)
             {
                 good = 0;
+                printf("bad inodes!\n");
             }
     }
 
@@ -341,14 +361,14 @@ int bitsCmpInodes(VDIFile *vdi, struct List *notReachable, struct List *reachabl
             if ((originalCurrentBit == 0) && (newCurrentBit == 1))
             {
                 uint32_t blockGroupIndex = byteNum * 8 + i;
-                uint32_t iNodeNumber = blockGroup * vdi->superBlock->inodesPerGroup + blockGroupIndex;
+                uint32_t iNodeNumber = blockGroup * vdi->superBlock->inodesPerGroup + blockGroupIndex + 1;
                 reachable->add(reachable, iNodeNumber);
                 count = 0;
             }
-            if ((originalCurrentBit) == 1 && (newCurrentBit == 0))
+            else if ((originalCurrentBit) == 1 && (newCurrentBit == 0))
             {
                 uint32_t blockGroupIndex = byteNum * 8 + i;
-                uint32_t iNodeNumber = blockGroup * vdi->superBlock->inodesPerGroup + blockGroupIndex;
+                uint32_t iNodeNumber = blockGroup * vdi->superBlock->inodesPerGroup + blockGroupIndex + 1;
                 notReachable->add(notReachable, iNodeNumber);
                 count = 0;
             }
@@ -367,6 +387,11 @@ int bitsCmpBlocks(VDIFile *vdi, struct List *notReachable, struct List *reachabl
         {
             uint32_t originalCurrentBit = (original >> i) & 1u;
             uint32_t newCurrentBit = (new >> i) & 1u;
+            if(byteNum == 135)
+            {
+                //printf("##############\n");
+                printf("byte 135 original bit: %d \t new bit: %d\n", originalCurrentBit, newCurrentBit);
+            }
             if ((originalCurrentBit == 0) && (newCurrentBit == 1))
             {
                 uint32_t blockGroupIndex = byteNum * 8 + i;
@@ -374,7 +399,7 @@ int bitsCmpBlocks(VDIFile *vdi, struct List *notReachable, struct List *reachabl
                 reachable->add(reachable, blockNum);
                 count = 0;
             }
-            if ((originalCurrentBit) == 1 && (newCurrentBit == 0))
+            else if ((originalCurrentBit) == 1 && (newCurrentBit == 0))
             {
                 uint32_t blockGroupIndex = byteNum * 8 + i;
                 uint32_t blockNum = blockGroup * vdi->superBlock->blocksPerGroup + blockGroupIndex;
@@ -406,8 +431,8 @@ Bitmaps* initializeBitmaps(VDIFile* vdi)
 
     for(size_t i = 0; i<vdi->superBlock->numBlockGroups; i++)
     {
-        bitmaps->iNodeBitmaps[i] = (uint8_t*)calloc(vdi->superBlock->blockSize, sizeof(uint8_t));
-        bitmaps->blockBitmaps[i] = (uint8_t*)calloc(vdi->superBlock->blockSize, sizeof(uint8_t));
+        bitmaps->iNodeBitmaps[i] = (uint8_t*)calloc(vdi->superBlock->blockSize, 1);
+        bitmaps->blockBitmaps[i] = (uint8_t*)calloc(vdi->superBlock->blockSize, 1);
     }
     return bitmaps;
 }
